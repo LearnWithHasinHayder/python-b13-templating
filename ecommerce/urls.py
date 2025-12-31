31f72f411f8434e06e3db6303d7bbb8eff5f6634
Hasin Hayder <countdraculla@gmail.com>
2025-12-31 21:40:48 +0530

    Django Views

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -1,5 +1,19 @@
 from django.urls import path
+from . import views
 
-app_name = 'ecommerce'
+app_name = "ecommerce"
 
-urlpatterns = []
+urlpatterns = [
+    path("check/<int:n>", views.num, name="odd_even"),
+    path("record/<str:name>/<int:age>", views.record, name="record"),
+    path("show/", views.show, name="show"),
+    path("users/", views.UserView.as_view(), name="users_view"),
+    path("users/<int:id>", views.UserDetailView.as_view(), name="user_view"),
+    path('json_users/', views.json_users, name='json_users'),
+    path('json_users/<int:id>', views.json_single_user, name='json_users')
+]
+
+# GET POST PUT PATCH DELETE
+
+#  /users GET POST 
+#  /users/1 GET PUT PATCH DELETE
--- ecommerce/views.py
+++ ecommerce/views.py
@@ -1,3 +1,76 @@
 from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse, Http404
+from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.http import require_http_methods, require_POST
+from django.utils.decorators import method_decorator
+
+from django.views import View
+import requests
+
 
 # Create your views here.
+def num(request, n):
+    isEven = n % 2 == 0
+    return render(request, "sample.html", {"n": n, "isEven": isEven})
+
+
+def record(request, name, age):
+    return HttpResponse(f"Name = {name} and age = {age}")
+
+def json_users(request):
+    try:
+        response = requests.get("https://jsonplaceholder.typicode.com/users")
+        data = response.json()
+    except requests.RequestException as e:
+        return JsonResponse({"error": "Failed to fetch data"}, status=500)
+
+    return JsonResponse(data, safe=False)
+
+def json_single_user(request, id):
+    try:
+        response = requests.get(f"https://jsonplaceholder.typicode.com/users/{id}")
+        data = response.json()
+    except requests.RequestException as e:
+        return JsonResponse({"error": "Failed to fetch data"}, status=500)
+
+    return JsonResponse(data, safe=False)
+
+@csrf_exempt
+# @require_http_methods(['GET','POST'])
+@require_POST
+def show(request):
+    return HttpResponse("done " + request.method)
+
+
+@method_decorator(csrf_exempt, name="dispatch")
+class UserView(View):
+    def get(self, request):
+        return JsonResponse({"message": "This is GET Call"})
+
+    def post(self, request):
+        return JsonResponse({"message": "This is POST Call"})
+
+    def put(self, request):
+        return JsonResponse({"message": "This is PUT Call"})
+
+    def patch(self, request):
+        return JsonResponse({"message": "This is PATCH Call"})
+
+    def delete(self, request):
+        return JsonResponse({"message": "This is DELETE Call"})
+
+@method_decorator(csrf_exempt, name="dispatch")
+class UserDetailView(View):
+    def get(self, request, id):
+        if(id>20):
+            raise Http404("User not found")
+        return JsonResponse({"message": f"This is GET Call for {id}"})
+    
+    def put(self, request, id):
+        return JsonResponse({"message": f"This is PUT Call for {id}"})
+
+    def patch(self, request, id):
+        return JsonResponse({"message": f"This is PATCH Call for {id}"})
+
+    def delete(self, request, id):
+        return JsonResponse({"message": f"This is DELETE Call for {id}"})
